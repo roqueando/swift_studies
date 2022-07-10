80f9ce6ab8dee01aa388809e0b8f32e01200be23
Vitor Roque <vitor.roquep@gmail.com>
2022-07-10 17:54:52 -0300

    Initial Commit

---
+++ ExampleTutorial/ExampleTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTutorialApp.swift
+//  ExampleTutorial
+//
+//  Created by Ayaworan on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
