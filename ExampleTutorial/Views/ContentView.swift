ee4212a19535d8f34ff1ed8d70f772321babc5f0
Vitor Roque <vitor.roquep@gmail.com>
2022-07-11 01:33:52 -0300

    Merge

---
+++ ExampleTutorial/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExampleTutorial
+//
+//  Created by Ayaworan on 10/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
